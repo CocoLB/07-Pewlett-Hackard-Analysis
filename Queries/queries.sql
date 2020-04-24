6ab4e0693b07101600a8b8bf51d1e9b54e448ebb
CocoLB <coco@cocolevant.com>
2020-04-24 13:57:28 -0700

    Adding retirement-by-title.csv Challenge Table 1

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -45,3 +45,124 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+DROP TABLE retiremen_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_fino and dept_emp
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+    de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no    
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retirement_dept_info
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+
+-- create table with retiree employee info and salaries
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+-- INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01')
+
+-- create table of managers retirees by dept
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+  --create tyable of retirees by dept
+SELECT ce.emp_no,
+    ce.first_name,
+    ce.last_name,
+    d.dept_name	
+INTO dept_info  
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no); 
+
+-- SKILL DRILL create table retirees in Sales dept
+SELECT ce.emp_no, ce.first_name, ce.last_name, d.dept_name
+INTO sales_retirement_info
+FROM retirement_info as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+LEFT JOIN departments as d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+
+-- SKILL DRILL create table retirees in Sale&Development dept
+SELECT ce.emp_no, ce.first_name, ce.last_name, d.dept_name
+INTO sales_development_retirement_info
+FROM retirement_info as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+LEFT JOIN departments as d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+
+------CHALLENGE-------
+
+--table for retiring employees by title and salaries, joining emp_info and titles
+SELECT ei.emp_no, ei.first_name, ei.last_name, t.title, t.from_date, ei.salary
+into retirement_by_title
+FROM emp_info ei
+JOIN titles t ON (ei.emp_no = t.emp_no AND ei.to_date = t.to_date)
+ORDER BY ei.emp_no;
+
+
+
+
+
+
